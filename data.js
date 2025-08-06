9bfbb2a620d7adf034e605b32d3c318be7ebc7b4
hurodigital <rpsanjaypaul@gmail.com>
2025-08-07 01:33:34 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,225 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-entrance",
+      "name": "Entrance",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 0.05739598000000257,
+        "pitch": 0,
+        "fov": 1.324777205633526
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.0047768099999991875,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "0-entrance"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-reception",
+      "name": "Reception",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 0.39333635999999217,
+        "pitch": 0,
+        "fov": 1.324777205633526
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "1-reception"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-reception",
+      "name": "Reception",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "2-reception"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-consult-room",
+      "name": "Consult Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 0.2304200500000011,
+        "pitch": 0,
+        "fov": 1.324777205633526
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "3-consult-room"
+        },
+        {
+          "yaw": 2.873099999999969,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "3-consult-room"
+        },
+        {
+          "yaw": 2.8863494699999643,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "1-reception"
+        },
+        {
+          "yaw": 0.2304200500000011,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "3-consult-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-consult-room",
+      "name": "Consult Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 0.5149418100000034,
+        "pitch": 0,
+        "fov": 1.324777205633526
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.011113209999925,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "0-entrance"
+        },
+        {
+          "yaw": 0.5149418100000034,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "4-consult-room"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
